91388c7c9b5ef65141c63e8f073eda061837f2a6
balen nouri <balennouri@gmail.com>
2024-02-05 21:30:25 +0000

    Feat: Add sign up form

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 
 # Create your views here.
@@ -34,3 +38,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You Have Been Logged Out!"))
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.Cleaned_data['username']
+            password = form.Cleaned_data['password1']
+            # Login the user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You Have Registered Successfully!! Welcome"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Whoops! There was a problem registering, Please try again."))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form},)
