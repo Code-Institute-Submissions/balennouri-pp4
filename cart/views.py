4ec7c2c5324ca720f598ef68a901aa4477246b95
balen nouri <balennouri@gmail.com>
2024-02-08 01:43:46 +0000

    Feat: Add cart quantity

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -7,8 +10,22 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
-
+    # Get the cart
+    cart = Cart(request)
+    # Test for POST
+    if request.POST.get('action') == 'post':
+        # Get the stuff
+        product_id = int(request.POST.get('product_id'))
+        # Lookup the product from database
+        product = get_object_or_404(Product, id=product_id)
+        # Save to a session
+        cart.add(product=product)
+        # Update the cart quantity
+        cart_quantity = cart.__len__()
+        # Return the response
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
