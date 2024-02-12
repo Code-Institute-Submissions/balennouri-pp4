e45985a42f3f6be8a6c77cd971c62d1834a9b46d
balen nouri <balennouri@gmail.com>
2024-02-12 17:13:01 +0000

    Add checkout page

---
+++ store/checkout.py
@@ -0,0 +1,9 @@
+from django.shortcuts import redirect, render
+from cart.cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+from django.contrib import messages
+
+
+def checkout_views(request):
+    return render(request, 'checkout.html')
