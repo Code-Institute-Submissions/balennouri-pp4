b815d89843a9e16663103f2e3ea4e165080ada89
balen nouri <balennouri@gmail.com>
2024-02-14 17:40:15 +0000

    Fix comments

--- store/models.py
+++ store/models.py
@@ -1,36 +1,6 @@
 from django.db import models
 import datetime
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
-
-
-class Profile(models.Model):
-    """
-    Customer profile for admin
-    """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_modified = models.DateTimeField(User, auto_now=True)
-    phone = models.CharField(max_length=20, blank=True)
-    address1 = models.CharField(max_length=200, blank=True)
-    address2 = models.CharField(max_length=200, blank=True)
-    city = models.CharField(max_length=200, blank=True)
-    country = models.CharField(max_length=200, blank=True)
-
-    def __str__(self):
-        return self.user.username
-
-
-def create_profile(sender, instance, created, **kwargs):
-    """
-    Creates a user profile by default when user sign up
-    """
-    if created:
-        user_profile = Profile(user=instance)
-        user_profile.save()
-
-
-post_save.connect(create_profile, sender=User)
 
 
 class Category(models.Model):
@@ -40,7 +10,6 @@ class Category(models.Model):
         return self.name
 
 
-# Customers Models
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -52,7 +21,6 @@ class Customer(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
-# All the product on the page
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
@@ -66,7 +34,6 @@ class Product(models.Model):
         return self.name
 
 
-# Customer Orders
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
