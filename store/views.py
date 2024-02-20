45b5107723e1148044178107b75b57cbb68ce8ea
balen nouri <balennouri@gmail.com>
2024-02-20 13:28:48 +0000

    Feat: Add comment to views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
+"""
+Here is the views for the website, most of the views are in this file.
+You can fin some views in the views in the cart app aswell.
+
+I got alot of insparations and ideas from this Youtube videos here:
+
+link 1: 'https://www.youtube.com/playlist?list=PL-
+51WBLyFTg0omnamUjL1TCVov7yDTRng'
+
+link 2: 'https://www.youtube.com/playlist?list=PL
+_KegS2ON4s53FNSqgXFdictTzUbGjoO-'
+"""
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.decorators import user_passes_test
 from .models import Product, Category, Comment
